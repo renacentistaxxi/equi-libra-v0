2c4778fb568d0ba28403dcbf2cbd2707e213ba12
Renacentista <renacentista@gmail.com>
2026-06-13 21:51:03 -0300

    Restore environment variables in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYT1Ptzfq14tWt86xTkgkLD077cYmXu_s",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
